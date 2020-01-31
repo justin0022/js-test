0447d21fb0c44ddf41af498943fc1f63709fb83a
Justin Lee <justinlee0022@gmail.com>
2020-01-31 15:36:44 -0800

    Added more tests

--- test/filter.test.js
+++ test/filter.test.js
@@ -3,5 +3,10 @@ const {
 } = require('../filter.js')
 
 test('filter out numbers great than 5 from array', () => {
-  expect(filterGreaterThan5([])).toEqual([])
+  expect(
+    filterGreaterThan5([])
+  ).toEqual([])
+  expect(
+    filterGreaterThan5([1, 2, 3, 4, 5, 6])
+  ).toEqual([6])
 })
--- test/map.test.js
+++ test/map.test.js
@@ -3,5 +3,10 @@ const {
 } = require('../map.js')
 
 test('double every number in array', () => {
-  expect(doubleNumbers([])).toEqual([])
+  expect(
+    doubleNumbers([])
+  ).toEqual([])
+  expect (
+    doubleNumbers([1, 2])
+  ).toEqual([2, 4])
 })
