445b8da5978f6dc37d2554d0112a5527898c2f71
Justin Lee <justinlee0022@gmail.com>
2020-01-31 16:17:13 -0800

    Many different ways to write the same thing

--- map.js
+++ map.js
@@ -1,6 +1,17 @@
-const doubleNumbers = array => {
-  return array.map(x => x * 2)
-}
+// const doubleNumber = x => x * 2
+const doubleNumbers = array => array.map(x => x * 2)
+
+// function doubleNumbers (array) {
+//   return array.map(function (x) {
+//     return x * 2
+//   })
+// }
+
+// function doubleNumbers (array) {
+//   const doubledNumbers = []
+//   array.forEach(x => doubledNumbers.push(x * 2))
+//   return doubledNumbers
+// }
 
 module.exports = {
   doubleNumbers
